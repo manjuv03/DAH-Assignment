fd995f1723a5d5aab03f8be23f143f576c10400e
Manju_Swaroop <manjuswaroop03@gmail.com>
2023-05-03 14:13:53 +0530

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,22 +1,5 @@
 #!/usr/bin/python
 
-""" 
-INPUT: File(s) containing words to count; these can also be passed to STDIN
-
-OUTPUT: E.g. (note the two occurrences of'the')
-            the 1
-            quick   1
-            brown   1
-            fox 1
-            jumped  1
-            over    1
-            the 1
-            lazy    1
-            grey    1
-            dogs    1
-"""
-
-
 import string
 import fileinput
 
